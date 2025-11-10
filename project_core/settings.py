1713459a2439fed3ee3e0b83d8fceb449b35f88e
Almog Ben baruch <almogbb74@gmail.com>
2025-11-10 11:02:46 +0200

    Edited .gitignore file

--- project_core/settings.py
+++ project_core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-from passwords import db_password
+from project_core.passwords import db_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
